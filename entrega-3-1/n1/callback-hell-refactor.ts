563abd7f720adde233f2f49f721b470af2d7ce22
Axel Piñeros <axelpineros@outlook.com>
2023-05-19 12:24:29 +0200

    Primera versió

--- entrega-3-1/n1/callback-hell-refactor.ts
+++ entrega-3-1/n1/callback-hell-refactor.ts
@@ -1,29 +1,18 @@
+// El codi adjunt llegeix un fitxer situat en un directori inbox i escriu el seu contingut invertit en un altre fitxer al directori outbox. Reestructura i simplifiqui el codi existent per a evitar el denominat Callback Hell.
+
 import { readdirSync, readFileSync, writeFileSync } from "fs";
-const readdir: typeof readdirSync = readdirSync;
-const readfile: typeof readFileSync = readFileSync;
-const writefile: typeof writeFileSync = writeFileSync;
-
 import { join } from "path";
-const joinPath: typeof join = join;
-
-const inbox: string = joinPath("inbox");
-const outbox: string = joinPath("outbox");
-
-const reverseText = (str: string) => str.split("").reverse().join("");
 
 try {
-    const files: string[] = readdir(inbox);
+    const reverseText = (str: string) => str.split("").reverse().join("");
+    const files: string[] = readdirSync("inbox");
     files.forEach((file) => {
-        const data = readfile(joinPath(inbox, file), "utf-8");
-        writefile(joinPath(outbox, file), reverseText(data));
+        const data = readFileSync(join("inbox", file), "utf-8");
+        writeFileSync(join("outbox", file), reverseText(data));
         console.log(`${file} s'ha guardat a la carpeta outbox!`);
     });
 }
 
 catch (error: unknown) {
-    if (error instanceof Error) {
-        console.log(`Error: ${error.message}`);
-    } else {
-        console.log(`Error: ${error}`);
-    }
+    error instanceof Error ? console.log(`Error: ${error.message}`) : console.log(`Error: ${error}`);
 }
