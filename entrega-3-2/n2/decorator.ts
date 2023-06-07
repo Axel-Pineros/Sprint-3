732f05a1d666d5215b7b150400934da117e05aa3
Axel Piñeros <axelpineros@outlook.com>
2023-06-07 17:12:14 +0200

    Primer intent decorator

--- entrega-3-2/n2/decorator.ts
+++ entrega-3-2/n2/decorator.ts
@@ -3,7 +3,7 @@
 import { readFileSync } from 'fs';
 
 // Decorador per a la conversió de moneda
- // @ts-ignore
+// @ts-ignore
 function currencyConversion(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
     const currencyConversions = JSON.parse(readFileSync('currency_conversions.json', 'utf-8'));
 
@@ -12,8 +12,8 @@ function currencyConversion(target: any, propertyKey: string, descriptor: Proper
 
     // Reescriu la funció per realitzar la conversió de moneda
     descriptor.value = function (...args: any[]) {
-        const amount = args[0];
-        const currency = args[1];
+        const amount:number = args[0];
+        const currency:string = args[1];
 
         if (!currencyConversions.hasOwnProperty(currency)) {
             throw new Error(`La divisa ${currency} no és compatible.`);
@@ -32,17 +32,35 @@ function currencyConversion(target: any, propertyKey: string, descriptor: Proper
 // Exemple de classe amb el mètode decorat
 class CurrencyConverter {
     @currencyConversion
-     // @ts-ignore
-    convertToEuros(amount: number, currency: string) {
+    // @ts-ignore
+    convertToEuros(amount: number, currency: string): number {
         // Aquest mètode serà decorat per realitzar la conversió de moneda
-        console.log('Convertint a euros...');
+        // console.log('Convertint a euros...');
         return amount;
     }
 }
 
 // Exemple d'ús
+// const converter = new CurrencyConverter();
+// converter.convertToEuros(100, 'USD_EUR'); // Aquesta crida imprimirà la conversió a la consola
+
+// Crea una petita aplicació que calculi el cost d'uns quants Articles en euros a partir de les seves divises inicials, aplicant diferents conversions que usin el Decorator del punt anterior.
+
+// Exemple d'ús
 const converter = new CurrencyConverter();
-converter.convertToEuros(100, 'USD_EUR'); // Aquesta crida imprimirà la conversió a la consola
+
+// Càlcul del cost d'articles en divises inicials
+const articles = [
+    { amount: 50, currency: 'USD_EUR' },
+    { amount: 80, currency: 'GBP_EUR' },
+    { amount: 120, currency: 'CHF_EUR' },
+    { amount: 1000, currency: 'JPY_EUR' },
+];
+
+articles.forEach((article) => {
+    const costInEuros:number = converter.convertToEuros(article.amount, article.currency);
+    console.log(`Cost de l'article: ${costInEuros} EUR`);
+});
 
 
 
